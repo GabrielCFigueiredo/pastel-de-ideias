c515bf5131065db5c14aef01d87fdd35850e9810
Gabriel <gabriel.devsistema@gmail.com>
2024-03-21 15:57:56 -0300

    iniciando e configurando projeto pastel de ideias

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BannerCard />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BannerCard from './components/BannerCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BannerCard
   }
 }
 </script>
---
+++ src/components/BannerCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <form action="" @submit.prevent="submitForm">
+    <input type="text" v-model="nome">
+    <button>Enviar</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'BannerCard',
+  data () {
+    return {
+      nome: ''
+    }
+  },
+  methods: {
+    submitForm () {
+      console.log(this.nome)
+    }
+  }
+}
+</script>
