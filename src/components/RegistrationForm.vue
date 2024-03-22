b147332ebc2b07e62a775c5b8ef31141a5b16fb1
Gabriel <gabriel.devsistema@gmail.com>
2024-03-22 11:05:37 -0300

    foi criado funções para cadastrar um pedido, limpar Campos,Selecionar tipo de imagem

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -3,14 +3,15 @@
     <form class="form-card-group" @submit.prevent="submitForm">
         <div class="card-title">
             <h3>Monte aqui seu cardápio. O que está esperando?</h3>
+            <input type="checkbox" v-model="isChecked" />
         </div>
         <div class="card-pastries">
             <img class="pastries" src="../../public/pastel.png" alt="">
         </div>
         <div class="card">
         <div class="card-input">
-            <input class="title-product" type="text" placeholder="Titúlo do Pedido" v-model="nome" required minlength="3" maxlength="60" />
-            <input class="flavor-product" type="text" placeholder="Sabor" v-model="sabor" required minlength="3" maxlength="60" />
+            <input class="title-product" type="text" placeholder="Titúlo do Pedido" v-model="name" required minlength="3" maxlength="60" />
+            <input class="flavor-product" type="text" placeholder="Sabor" v-model="flavor" required minlength="3" maxlength="60" />
             <input class="price-product" type="number" placeholder="R$" v-model="price" required />
         </div>
         <div class="card-description">
@@ -24,10 +25,10 @@
         <br>
         <div class="card-button">
             <div>
-                <button class="btn-cancel">Cancelar</button>
+                <button class="btn-cancel" @click="cancelForm" type="submit">Cancelar</button>
             </div>
             <div>
-                <button class="btn-register">Cadastrar</button>
+                <button class="btn-register" type="submit">Cadastrar</button>
             </div>
         </div>
     </div>
@@ -49,16 +50,19 @@
 <br>
 <br>
 <br>
-<div class="card-order">
+<div class="card-order"  v-for="product in reversedResult" :key="product">
     <div>
         <h1>Aqui está o seu pedidos Bom Apetite!</h1>
     </div>
     <div class="card-title-order">
         <div class="order">
-            <h3 class="product-request-title">{{ nome }}</h3>
-            <h3 class="asking-price">R$ {{ price }}</h3>
+            <h3 class="product-request-title">{{ product.name }}</h3>
+            <h3 class="asking-price">R$ {{ product.price }}</h3>
         </div>
-        <h3>Sabor: {{ sabor }}</h3>
+        <div>
+        <h3>Sabor: {{ product.flavor }}</h3>
+        <h3>Descrição: {{ product.description }}</h3>
+        </div>
     </div>
 </div>
 </template>
@@ -68,18 +72,31 @@ export default {
   data () {
     return {
       result: [],
-      nome: '',
-      sabor: '',
+      name: '',
+      flavor: '',
       price: '',
       description: '',
       previewImage: null
     }
   },
+  computed: {
+    reversedResult () {
+      return this.result.slice().reverse()
+    }
+  },
+
   methods: {
-    submitForm () {
-      this.result.push(this.nome, this.sabor, this.price, this.description)
-      this.nome = ''
-      this.sabor = ''
+    async submitForm () {
+      const data = {
+        name: this.name,
+        flavor: this.flavor,
+        price: this.price,
+        description: this.description
+      }
+      this.result.push(data)
+      this.clear()
+      this.name = ''
+      this.flavor = ''
       this.price = ''
       this.description = ''
     },
@@ -89,14 +106,30 @@ export default {
     pickFile () {
       const input = this.$refs.fileInput
       const file = input.files
+      const typeImage = ['image/png', 'image/jpeg']
       if (file && file[0]) {
-        const reader = new FileReader()
-        reader.onload = e => {
-          this.previewImage = e.target.result
+        const fileTypes = file[0].type
+        if (typeImage.includes(fileTypes)) {
+          const reader = new FileReader()
+          reader.onload = e => {
+            this.previewImage = e.target.result
+          }
+          reader.readAsDataURL(file[0])
+          this.$emit('input', file[0])
+        } else {
+          alert('Por favor, selecione um arquivo PNG ou JPG.')
         }
-        reader.readAsDataURL(file[0])
-        this.$emit('input', file[0])
       }
+    },
+    cancelForm () {
+      this.clear()
+    },
+    clear () {
+      this.name = ''
+      this.flavor = ''
+      this.price = ''
+      this.description = ''
+      this.previewImage = null
     }
   }
 }
@@ -262,15 +295,13 @@ appearance: none;
 }
 
 .card-order {
-    border: 1px solid #E53F36;
     display: flex;
     flex-direction: column;
     align-items: center;
     width: 100%;
-    height: 50vh;
+    height: 30vh;
 }
 .card-title-order {
-    border: 1px solid rebeccapurple;
     width: 50%;
     height: 30%;
 }
